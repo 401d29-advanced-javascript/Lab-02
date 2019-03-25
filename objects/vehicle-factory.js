ace40ec9c8a7e1c355f6ec209454ccf2a07a2ef2
AaronBruce <aaron.bruce@comcast.net>
2019-03-24 19:51:51 -0700

    added constructors for motorcycle and car

--- objects/vehicle-factory.js
+++ objects/vehicle-factory.js
@@ -1,8 +1,30 @@
 'use strict';
 
-function CarFactory() {
-}
+CarFactory = (name) => {
+  let car = Object.assign({}, {name}, {drive}, {stop});
+  let drive = () => {
+    return 'Moving Forward';
+  };
+  let stop = () => {
+    return 'Stopping';
+  };
+  return Object.freeze(car);
+};
 
-module.exports = CarFactory;
+MotorcycleFactory = (name) => {
+  let motorcycle = Object.assign({}, {name}, {drive}, {stop}, {wheelie});
+  let drive = () => {
+    return 'Moving Forward';
+  };
+  let stop = () => {
+    return 'Stopping';
+  };
+  let wheelie = () => {
+    return 'Wheee!';
+  };
+  return Object.freeze(car);
+};
+
+module.exports = {CarFactory, MotorcycleFactory};
 
 
